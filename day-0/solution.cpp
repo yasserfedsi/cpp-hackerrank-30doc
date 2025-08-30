897775ee5496f9b6952350301fc696448c6a3454
Yasser <yasserfedsi34@gmail.com>
2025-08-30 15:52:39 +0200

    first commit

---
+++ day-0/solution.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    string text;
+    getline(cin, text);
+
+    cout << "Hello, World." << endl;
+    cout << text << endl;
+
+    std::cout << "";
+    return 0;
+}
